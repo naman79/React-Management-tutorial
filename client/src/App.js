e31b26838b1b3fef93e994a1a9aed498ce8fa73c
minsukang <naman79@kaoni.com>
2020-07-29 02:18:18 +0900

    add cors and mysql

--- client/src/App.js
+++ client/src/App.js
@@ -1,89 +1,67 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Customer from './components/Customer'
 import './App.css';
-import Customer from './components/Customer';
-import Table from '@material-ui/core/table';
+import Paper from '@material-ui/core/Paper';
+import Table from '@material-ui/core/Table';
 import TableHead from '@material-ui/core/TableHead';
 import TableBody from '@material-ui/core/TableBody';
 import TableRow from '@material-ui/core/TableRow';
 import TableCell from '@material-ui/core/TableCell';
-import { makeStyles } from '@material-ui/core/styles';
-import Paper from '@material-ui/core/Paper';
-import TableContainer from '@material-ui/core/TableContainer';
+import { withStyles } from '@material-ui/core/styles';
 
-const styles = makeStyles({
+const styles = theme => ({
   root: {
-    width: '100%',
+    width: "100%",
     spacing: value => value ** 2,
     overflowX: "auto"
   },
   table: {
     minWidth: 1080
   }
-})
+});
 
-const customers = [
-  {
-  'id' : 1,
-  'image' : 'https://placeimg.com/64/64/any',
-  'name' : '홍길동',
-  'birthday' : '961111',
-  'gender' : '남자',
-  'job' : '대학생'
-},
-{
-  'id' : 2,
-  'image' : 'https://placeimg.com/64/64/2',
-  'name' : '스티븐',
-  'birthday' : '691113',
-  'gender' : '남자',
-  'job' : '개발자'
-},
-{
-  'id' : 3,
-  'image' : 'https://placeimg.com/64/64/3',
-  'name' : '이순신',
-  'birthday' : '761212',
-  'gender' : '남자',
-  'job' : '디자이너'
-}
-]
+class App extends Component {
 
-function App() {
-  const classes = styles();
-  return (
-    <TableContainer component={Paper} className={classes.root}>
-      <Table className={classes.table}>
-        <TableHead>
-        <TableRow>
-          <TableCell>번호</TableCell>
-          <TableCell>이미지</TableCell>
-          <TableCell>이름</TableCell>
-          <TableCell>생년월일</TableCell>
-          <TableCell>성별</TableCell>
-          <TableCell>직업</TableCell>
-        </TableRow>
-      </TableHead>
-      <TableBody>
-        {
-          customers.map(c => {
-            return (
-                  <Customer
-                  key={c.id}
-                  id={c.id}
-                  image={c.image}
-                  name={c.name}
-                  birthday={c.birthday}
-                  gender={c.gender}
-                  job={c.job}
-                />  
-            )
-          })
-        }   
-        </TableBody>
-      </Table>
-    </TableContainer>
-  );
+  state = {
+    customers: []
+  }
+
+  componentDidMount() {
+    this.callApi()
+      .then(res => this.setState({customers: res}))
+      .catch(err => console.log(err));
+  }
+
+  callApi = async () => {
+    const response = await fetch('http://localhost:5000/api/customers');
+    const body = await response.json();
+    return body;
+  }
+
+  render() {
+    const { classes } = this.props;
+    return (
+      <Paper className={classes.root}>
+        <Table className={classes.table}>
+          <TableHead>
+            <TableRow>
+              <TableCell>번호</TableCell>
+              <TableCell>이미지</TableCell>
+              <TableCell>이름</TableCell>
+              <TableCell>생년월일</TableCell>
+              <TableCell>성별</TableCell>
+              <TableCell>직업</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {this.state.customers ? this.state.customers.map(c => {
+              return <Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job} />
+            }) : ''}
+          </TableBody>
+        </Table>
+      </Paper>
+    );
+  }
 }
 
-export default App;
+export default withStyles(styles)(App);
--- server.js
+++ server.js
@@ -1,12 +1,42 @@
+const fs = require('fs');
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
-const port = process.env.PORT || 5000;
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended:true}));
+const cors = require('cors');
 
-app.get('/api/hello', (req, res) => {
-    res.send({ message: 'Hello Express!'});
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const corsOptions = {
+    origin: 'http://localhost:3000', // 허락하고자 하는 요청 주소
+    credentials: true, // true로 하면 설정한 내용을 response 헤더에 추가 해줍니다.
+};
+
+const data = fs.readFileSync('./database.json');
+const conf = JSON.parse(data);
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+host: conf.host,
+user: conf.user,
+password: conf.password,
+port: conf.port,
+database: conf.database
+});
+
+connection.connect();
+
+app.get('/api/customers', cors(corsOptions),(req, res) => {
+
+connection.query(
+'SELECT * FROM CUSTOMER',
+(err, rows, fields) => {
+res.send(rows);
 })
 
-app.listen(port, () => console.log('Listening on port ${port}'));
+});
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
